0e73a95cfa97a5a81e3c6930cdf22121eed87138
smitbahadure1 <bahaduresmit08@gmail.com>
2025-09-26 00:01:15 +0530

    Create serviceImages.ts

---
+++ src/utils/serviceImages.ts
@@ -0,0 +1,5 @@
+// Your uploaded plumbing repair image
+// Save your uploaded image as: public/plumbing-repair.jpg
+// The system will use this image for the plumbing repair service
+
+export const plumbingRepairImage = '/plumbing-repair.jpg';
